ccf200191e5ee75fd533493fbca4aad67764e4e2
matheus <matheusrodrig16@gmail.com>
2024-11-26 18:30:00 -0300

    feature: implement user and computer scores

--- main.py
+++ main.py
@@ -12,5 +12,20 @@ for _ in range(2):
     user_hand.append(deal_card())
     computer_hand.append(deal_card())
 
+def calculate_score(cards):
+    score = sum(cards)
+    if score == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and score > 21: # quando tiver dois Áses
+        cards.remove(11)
+        
+    return sum(cards)
+
+user_score = calculate_score(user_hand)
+computer_score = calculate_score(computer_hand)
+
 print(user_hand)
+print(user_score)
+
 print(computer_hand)
+print(computer_score)
